8e0436d391c2556f71220e5fa61d33a5d749244a
Cristian <advanced.development.solutions@gmail.com>
2019-01-20 18:27:11 -0500

    Ensure library folder is created by script.bat

--- src/java/script.bat
+++ src/java/script.bat
@@ -2,6 +2,7 @@ del ..\..\library\ultimatebox.jar
 rmdir /S ..\..\reference
 rmdir /S bin\com
 mkdir bin
+mkdir ..\..\library
 javac -d bin -cp C:\mySandBox\AppsBinFolder\P3\current\processing\core\library\core.jar com\UltimateBox\*.java -verbose
 jar -cf ultimatebox.jar -C bin/ . && move ultimatebox.jar ..\..\library\
 javadoc -cp C:\mySandBox\AppsBinFolder\P3\current\processing\core\library\core.jar -sourcepath com\UltimateBox -d reference com.UltimateBox com\UltimateBox\*.java && move reference ..\..\
